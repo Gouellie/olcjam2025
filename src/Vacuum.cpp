8c333437001efa13984a84fa793740fc9c3d2eda
Guillaume Ouellet <gulaou@gmail.com>
2025-08-25 22:38:37 -0400

    Basic vacuum logic in

--- src/Vacuum.cpp
+++ src/Vacuum.cpp
@@ -19,4 +19,21 @@ void Vacuum::Update(const orxCLOCK_INFO &_rstInfo)
 
 void Vacuum::OnCollide(ScrollObject* _poCollider, orxBODY_PART* _pstPart, orxBODY_PART* _pstColliderPart, const orxVECTOR& _rvPosition, const orxVECTOR& _rvNormal) 
 {
+    PushConfigSection();
+    orxFLOAT vacuumStrenght = orxConfig_GetFloat("Strenght");
+    PopConfigSection();
+
+    orxVECTOR vVacuumOrigin, vColliderPosition, vDirection;
+    GetPosition(vVacuumOrigin, orxTRUE);
+
+    _poCollider->GetPosition(vColliderPosition, orxTRUE);
+
+    orxVector_Sub(&vDirection, &vVacuumOrigin, &vColliderPosition);
+
+    orxVector_Normalize(&vDirection, &vDirection);
+
+    orxVector_Mulf(&vDirection, &vDirection, vacuumStrenght);
+
+    orxObject_ApplyImpulse(_poCollider->GetOrxObject(), &vDirection, orxNULL);
+
 }
