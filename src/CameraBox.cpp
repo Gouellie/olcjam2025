55818456ec690330ce2b98e3f2369f20be63e225
RENCURELI <pierre.rencurel@orange.fr>
2025-08-25 21:26:16 -0400

    Basic CameraBox setup, saving for tomorrow

---
+++ src/CameraBox.cpp
@@ -0,0 +1,23 @@
+#include "CameraBox.h"
+
+
+void CameraBox::OnCreate()
+{
+
+}
+
+void CameraBox::OnDelete()
+{
+
+}
+
+void CameraBox::Update(const orxCLOCK_INFO& _rstInfo)
+{
+	// Special case for "mining" activity
+
+	// Check if player is outside of bounds
+
+	// Set target position based on where player is
+
+	// Lerp Camera towards target position
+}
